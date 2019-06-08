cfd814746509fe4eef70aacae1660477930950df
akitanak <akihito_tanaka@me.com>
2019-06-08 23:53:20 +0900

    add CREATION extension test variation.

--- test_api.py
+++ test_api.py
@@ -44,6 +44,31 @@ def test_creation_extention_can_deferred_the_length_of_upload(api):
     assert path.parent == Path('/files')
     assert_uuid_format(path.name)
 
+def test_creation_extention_response_400_when_upload_defer_length_value_is_not_1(api):
+    """
+    CREATION extention response 400, when Upload-Defer-Length header value is not 1.
+    """
+    headers = {
+        'Upload-Defer-Length': '0',
+        'Tus-Resumable': '1.0.0'
+    }
+
+    resp = api.requests.post(f'/files', headers=headers)
+
+    assert resp.status_code == 400
+
+def test_creation_extension_response_400_when_upload_length_and_upload_defer_length_are_not_specified(api):
+    """
+    CREATION extention response 400, when Upload-Length and Upload-Defer-Length header is not specified.
+    """
+    headers = {
+        'Tus-Resumable': '1.0.0'
+    }
+
+    resp = api.requests.post(f'/files', headers=headers)
+
+    assert resp.status_code == 400    
+
 def test_head_request_response_upload_offset_when_resource_exists(api):
     """
     HEAD request responses Upload-Offset header, if resource exists.
