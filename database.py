96baa6e0f13ba6ad1412201ebdf171fc133f2b84
akitanak <akihito_tanaka@me.com>
2019-06-15 15:02:23 +0900

    add upload_concat to database

--- database.py
+++ database.py
@@ -7,15 +7,17 @@ class UploadData:
         "upload_offset",
         "upload_length",
         "upload_defer_length",
-        "upload_metadata"
+        "upload_metadata",
+        "upload_concat"
     ]
 
-    def __init__(self, upload_length=None, upload_defer_length=None, metadata={}):
+    def __init__(self, upload_length=None, upload_defer_length=None, metadata={}, upload_concat=None):
         self.id = uuid4()
         self.upload_offset = 0
         self.upload_length = upload_length
         self.upload_defer_length = upload_defer_length
         self.upload_metadata = metadata
+        self.upload_concat = upload_concat
 
 
 class Database:
@@ -23,8 +25,8 @@ class Database:
     def __init__(self):
         self.uploads = {}
 
-    def add_uploads(self, upload_length=None, upload_defer_length=None, metadata={}):
-        upload_data = UploadData(upload_length, upload_defer_length, metadata)
+    def add_uploads(self, upload_length=None, upload_defer_length=None, metadata={}, upload_concat=None):
+        upload_data = UploadData(upload_length, upload_defer_length, metadata, upload_concat)
         self.uploads[upload_data.id] = upload_data
 
         return upload_data
--- test_database.py
+++ test_database.py
@@ -14,8 +14,9 @@ def test_add_uploads_save_new_upload_data(database):
         'key1': 'value1',
         'key2': 'value2'
     }
+    upload_concat = 'partial'
 
-    data = database.add_uploads(upload_length=upload_length, metadata=metadata)
+    data = database.add_uploads(upload_length=upload_length, metadata=metadata, upload_concat=upload_concat)
 
     saved = database.uploads.get(data.id)
 
@@ -23,6 +24,7 @@ def test_add_uploads_save_new_upload_data(database):
     assert saved.upload_length == upload_length
     assert saved.upload_defer_length is None
     assert saved.upload_metadata == metadata
+    assert saved.upload_concat == upload_concat
 
 
 def test_get_by_id_returns_matched_upload_data(database):
@@ -31,8 +33,9 @@ def test_get_by_id_returns_matched_upload_data(database):
         'key1': 'value1',
         'key2': 'value2'
     }
+    upload_concat = 'final'
 
-    data = database.add_uploads(upload_length=upload_length, metadata=metadata)
+    data = database.add_uploads(upload_length=upload_length, metadata=metadata, upload_concat=upload_concat)
 
     retrieved = database.get_by_id(data.id)
 
@@ -40,6 +43,7 @@ def test_get_by_id_returns_matched_upload_data(database):
     assert retrieved.upload_length == upload_length
     assert retrieved.upload_offset == 0
     assert retrieved.upload_metadata == metadata
+    assert retrieved.upload_concat == upload_concat
 
 
 def test_set_upload_length(database):
